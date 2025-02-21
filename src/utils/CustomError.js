6c9f75f75e7476696dcc4e6e61940addb11a9adc
tikamkasper <tikamkasper@gmail.com>
2025-02-21 08:27:26 +0530

    imporve_err_handling

--- src/utils/CustomError.js
+++ src/utils/CustomError.js
@@ -1,9 +1,8 @@
 class CustomError extends Error {
-  constructor(statusCode, message) {
-    super(message); // Call parent constructor (Error)
-    this.statusCode = statusCode;
+  constructor(statusCode, message = "Something went wrong.") {
+    super(); // Call parent constructor (Error)
+    this.statusCode = statusCode; // Custom status code
+    this.message = message; // Custom error message
     Error.captureStackTrace(this, this.constructor); // Optional for cleaner stack traces
   }
 }
-
-module.exports = CustomError;
--- src/utils/Response.js
+++ src/utils/Response.js
@@ -1,24 +1,34 @@
+const { NODE_ENV } = require("../config");
+
 class Response {
-  static success(res, statusCode = 200, data = {}, message = "Success") {
+  static success(
+    res,
+    statusCode = 200,
+    message = "Request successful.",
+    data = {}
+  ) {
     return res.status(statusCode).json({
       statusCode,
-      data,
-      message,
       success: true,
+      message,
+      data,
     });
   }
 
   static error(
     res,
     statusCode = 500,
-    message = "Something went wrong",
+    message = statusCode === 500
+      ? "Internal Server Error. Please try again later."
+      : "An error occurred while processing your request.",
     errors = []
   ) {
     return res.status(statusCode).json({
       statusCode,
-      message,
-      errors,
       success: false,
+      message,
+      ...(NODE_ENV === "development" && originalErrorMessage),
+      ...(NODE_ENV === "development" && errors),
     });
   }
 }
